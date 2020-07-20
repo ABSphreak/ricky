4b9fffc66442630917f384545560951afaf4a81f
ᴀʙʜɪɴᴀᴠ sʜᴀʀᴍᴀ✩ <absphreak@outlook.com>
2020-07-20 23:23:29 +0530

    ➕ init node code

---
+++ index.js
@@ -0,0 +1,43 @@
+// For setting up your bot token
+require("dotenv").config();
+const Discord = require("discord.js");
+const token = process.env.TOKEN;
+const PREFIX = "!";
+
+// Initializing Bot Client
+const bot = new Discord.Client();
+
+// If the bot is ready then print this message
+bot.on("ready", () => {
+  console.log(`Logged in as → ${bot.user.tag}`);
+});
+
+bot.on("message", (msg) => {
+  // if (msg.content === "hello") {
+  //   msg.reply("Hello!");
+  // }
+  let args = msg.content.substring(PREFIX.length).split(" ");
+  switch (args[0]) {
+    case "ping":
+      msg.channel.send("pong!");
+      break;
+    case "website":
+      msg.channel.send("https://abhinav.sh/");
+      break;
+    case "info":
+      if (args[1] === "version") {
+        msg.channel.send("v1.0.0");
+      } else {
+        msg.channel.send("Invalid Arguments!");
+      }
+      break;
+    case "clear":
+      if (!args[1]) {
+        return msg.reply("Error! Please define a second argument.");
+      }
+    default:
+      msg.channel.send("You typed something wrong!");
+  }
+});
+
+bot.login(token);
